c44ddc652d845d9b5d3d1a3e478c7e0652935215
Carles Genoher <genoher@gmail.com>
2016-04-19 16:34:28 +0200

    - Añadir pagina de Login, LoginViewModel

---
+++ NoteApps/NoteApps/Scripts/ViewModels/LoginViewModel.js
@@ -0,0 +1,12 @@
+﻿LoginViewModel = function () {
+    var self = this;
+    self.User;
+    self.Password;
+
+    self.Login = function () {
+        alert("No implementado");
+    }
+}
+
+angular.module("NotesApp")
+       .controller("LoginViewModel", LoginViewModel);
