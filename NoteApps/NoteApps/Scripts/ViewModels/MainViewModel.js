3a8445c08d57d750187b75f21cf89e6cd580e79a
Carles Genoher <genoher@gmail.com>
2016-04-19 15:41:14 +0200

    Añadir imput bindado al titulo, y añadir boton con una accion de cambiar el titulo.

--- NoteApps/NoteApps/Scripts/ViewModels/MainViewModel.js
+++ NoteApps/NoteApps/Scripts/ViewModels/MainViewModel.js
@@ -1,6 +1,10 @@
 ﻿MainViewModel = function () {
     var self = this;
     self.Title = "Qué pasa tronco!!";
+
+    self.ChangeTitle = function() {
+        self.Title = "hola caracola";
+    }
 }
 
 angular.module("NotesApp")
